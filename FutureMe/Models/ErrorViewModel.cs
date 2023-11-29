81376d28425eeb51176e0cca8df46e8d4162b0ca
Younes Makhlouf <younes.makhlouf@insat.ucar.tn>
2023-11-29 13:29:25 +0100

    Add project files.

---
+++ FutureMe/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FutureMe.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

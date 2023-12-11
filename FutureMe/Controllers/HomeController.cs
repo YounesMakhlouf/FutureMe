5b1fe2e0a0da653b8d3dd6546924f2697d137372
chadda <chadha.essid@insat.ucar.tn>
2023-12-11 23:33:16 +0100

    feature: save letter *write Letter Repository and letter service *add SaveLetter() action in homeController

--- FutureMe/Controllers/HomeController.cs
+++ FutureMe/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using FutureMe.Models;
+using FutureMe.Services.LetterSaver;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,10 +8,12 @@ namespace FutureMe.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ILetterSaverService _letterSaverService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,ILetterSaverService letterSaverService)
         {
             _logger = logger;
+            _letterSaverService = letterSaverService;
         }
 
         public IActionResult Index()
@@ -28,5 +31,22 @@ namespace FutureMe.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult saveLetter(string content, DateTime sendingDate, bool IsPublic, string Email, string Title, int UserId)
+        {
+            if (!ModelState.IsValid)
+            {
+                return (RedirectToAction(nameof(Index)));
+            }
+
+            _letterSaverService.saveLetter(content, sendingDate, IsPublic, Email, Title, 1); //change 1 to UserId once identity is included
+            //TODO:Add success message to view
+            return (RedirectToAction(nameof(Index)));
+
+
+        }
+
+
     }
 }
--- FutureMe/Program.cs
+++ FutureMe/Program.cs
@@ -1,11 +1,19 @@
+using FutureMe.Models;
+using FutureMe.Repositories;
 using FutureMe.Services.EmailSender;
+using FutureMe.Services.LetterSaver;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddTransient<IEmailSender, EmailSender>();
-
+builder.Services.AddScoped<LetterRepository, LetterRepository>();
+builder.Services.AddScoped<IEmailSender, EmailSender>();
+builder.Services.AddTransient<ILetterSaverService, LetterSaverService>();
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")
+));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.

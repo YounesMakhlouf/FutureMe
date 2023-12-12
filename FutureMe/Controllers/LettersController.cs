f5042ac499c2c7425f43dfd7a93a487d80b57877
Younes Makhlouf <younes.makhlouf@insat.ucar.tn>
2023-12-12 16:08:01 +0100

    Removed Delete and Edit actions and views

--- FutureMe/Controllers/LettersController.cs
+++ FutureMe/Controllers/LettersController.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using FutureMe.Models;
 
@@ -64,99 +59,6 @@ namespace FutureMe.Controllers
                 return RedirectToAction(nameof(Index));
             }
             return View(letter);
-        }
-
-        // GET: Letters/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null || _context.Letters == null)
-            {
-                return NotFound();
-            }
-
-            var letter = await _context.Letters.FindAsync(id);
-            if (letter == null)
-            {
-                return NotFound();
-            }
-            return View(letter);
-        }
-
-        // POST: Letters/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Content,WritingDate,SendingDate,IsPublic,Email,Title")] Letter letter)
-        {
-            if (id != letter.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(letter);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!LetterExists(letter.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(letter);
-        }
-
-        // GET: Letters/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null || _context.Letters == null)
-            {
-                return NotFound();
-            }
-
-            var letter = await _context.Letters
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (letter == null)
-            {
-                return NotFound();
-            }
-
-            return View(letter);
-        }
-
-        // POST: Letters/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            if (_context.Letters == null)
-            {
-                return Problem("Entity set 'AppDbContext.Letters'  is null.");
-            }
-            var letter = await _context.Letters.FindAsync(id);
-            if (letter != null)
-            {
-                _context.Letters.Remove(letter);
-            }
-            
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool LetterExists(int id)
-        {
-          return (_context.Letters?.Any(e => e.Id == id)).GetValueOrDefault();
         }
     }
 }

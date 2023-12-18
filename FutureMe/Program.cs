10399d0620d38d66300dee4197b600a42e1aa64c
chadda <chadha.essid@insat.ucar.tn>
2023-12-18 15:43:38 +0100

    feature : register & login

--- FutureMe/Program.cs
+++ FutureMe/Program.cs
@@ -3,11 +3,20 @@ using FutureMe.Repositories;
 using FutureMe.Services.EmailSender;
 using FutureMe.Services.LetterSaver;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using FutureMe.Data;
+using FutureMe.Areas.Identity.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<DataContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")
+));
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<DataContext>();
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 builder.Services.AddScoped<LetterRepository, LetterRepository>();
 builder.Services.AddScoped<IEmailSender, EmailSender>();
 builder.Services.AddTransient<ILetterSaverService, LetterSaverService>();
@@ -28,11 +37,13 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();;
 
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
 
 app.Run();

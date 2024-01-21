7e13b5d7a75b55b4db53a51409004c4c3b814a24
Selma Bouabidi <selma.bouabidi@insat.ucar.tn>
2024-01-21 23:13:01 +0100

    Added job functionality for retrieving and sending emails.

--- FutureMe/BackgroundJobs/EmailSendingBackgroundJob.cs
+++ FutureMe/BackgroundJobs/EmailSendingBackgroundJob.cs
@@ -1,6 +1,8 @@
 ﻿using FutureMe.Models;
 using FutureMe.Services.EmailSender;
+using Microsoft.EntityFrameworkCore;
 using Quartz;
+using Quartz.Impl.AdoJobStore.Common;
 using System.Diagnostics.Metrics;
 
 namespace FutureMe.BackgroundJobs
@@ -9,34 +11,42 @@ namespace FutureMe.BackgroundJobs
     public class EmailSendingBackgroundJob : IJob
     {
         private IEmailSender _sender;
-        public  EmailSendingBackgroundJob(IEmailSender sender)
+        private AppDbContext _appDbContext;
+        private readonly IServiceProvider _provider;
+
+        public EmailSendingBackgroundJob(IEmailSender sender,IServiceProvider provider)
         {
             _sender = sender;
+            _provider = provider;
         }
         public Task Execute(IJobExecutionContext context)
         {
             try
             {
-                Console.WriteLine("test");
-                //var letters = GetLetters();
-                //letters.ForEach(letter =>
-                //{
-                //   _sender.SendEmailAsync(letter.Email, letter.Title, letter.Content);
-                //});
-                _sender.SendEmailAsync("salmabouabidi019@gmail.com","test", "test");
+                using (var scope = _provider.CreateScope())
+                {
+                    _appDbContext = scope.ServiceProvider.GetService<AppDbContext>();
+
+                    var letters = GetLetters();
+                    letters.ForEach(letter =>
+                    {
+                        _sender.SendEmailAsync(letter.Email, letter.Title, letter.Content);
+                    });
+                }
             }
-            catch(JobExecutionException e) {
+            catch (JobExecutionException e)
+            {
                 throw new JobExecutionException();
 
             }
             return Task.CompletedTask;
         }
         // Temporary function until we merge other branches
-        //public List<Letter> GetLetters()
-        //{
-        //   return _appDbContext.Letters
-        //        .Where(letter => letter.SendingDate.Date == DateTime.Today)
-        //        .ToList();
-        //}
+        public List<Letter> GetLetters()
+        {
+           return _appDbContext.Letters
+                .Where(letter => letter.SendingDate.Date == DateTime.Today)
+                .ToList();
+        }
     }
 }
--- FutureMe/Program.cs
+++ FutureMe/Program.cs
@@ -9,14 +9,14 @@ using QuartzHostedService = FutureMe.BackgroundJobs.QuartzHostedService;
 
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddTransient<IEmailSender, EmailSender>();
 builder.Services.AddSingleton<EmailSendingBackgroundJob>();
 builder.Services.AddSingleton<IJobFactory, JobFactory>();
 builder.Services.AddSingleton<BackgroundJobScheduler>();
 builder.Services.AddHostedService<QuartzHostedService>();
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<AppDbContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 // Add Quartz services
 builder.Services.AddSingleton<ISchedulerFactory, StdSchedulerFactory>();
 

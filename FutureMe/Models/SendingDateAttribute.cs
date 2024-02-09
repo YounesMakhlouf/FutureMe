3614786d1a1319c755f1e2ad7c73765691ac25c8
Younes Makhlouf <younes.makhlouf@insat.ucar.tn>
2024-02-09 08:49:31 +0100

    Validation now only takes into account the date (not the time)

--- FutureMe/Models/SendingDateAttribute.cs
+++ FutureMe/Models/SendingDateAttribute.cs
@@ -7,7 +7,7 @@ namespace FutureMe.Models
             public override bool IsValid(object value)// Return a boolean value: true == IsValid, false != IsValid
             {
                 DateTime d = Convert.ToDateTime(value);
-                return d >= DateTime.Now; //Dates Greater than or equal to today are valid (true)
+                return d.Date >= DateTime.Now.Date; //Dates Greater than or equal to today are valid (true)
             }
         }
 }
